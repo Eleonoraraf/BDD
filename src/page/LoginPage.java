a0032586d28d49925fc4728412bbfa7bee4b1f19
“Eleonora” <rahovskaaeleonoraf@gmail.com>
2024-03-10 18:38:31 +0300

    first commit

---
+++ src/page/LoginPage.java
@@ -0,0 +1,17 @@
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+import static com.codeborne.selenide.Selenide.$;
+
+public class LoginPage {
+
+    private final SelenideElement loginField = $("[data-test-id=login] input");
+    private final SelenideElement passwordField = $("[data-test-id=password] input");
+    private final SelenideElement loginButton = $("[data-test-id=action-login]");
+
+    public VerificationPage validLogin(DataHelper.AuthInfo info){
+        loginField.setValue(info.getLogin());
+        passwordField.setValue(info.getPassword());
+        loginButton.click();
+        return new VerificationPage();
+    }
+}
